2d23f334d4261eb5193eaab964c55abcf374575e
anujachaitanya <58026823+anujachaitanya@users.noreply.github.com>
2020-01-03 22:22:32 +0530

    modified sort.js

--- sort.js
+++ sort.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-const { stdin , stdout, stderr} = require('process')
-const {  parseUserArgs } = require('./src/optionParsing');
-const { sort } = require('./src/executeSort');
+const {stdin, stdout, stderr} = require('process');
+const {parseUserArgs} = require('./src/optionParsing');
+const {sort} = require('./src/executeSort');
 const [, , ...cmdLineArgs] = process.argv;
 
 const main = function () {
-  const options = parseUserArgs(cmdLineArgs);;
-  sort(options, stdin, fs.createReadStream, ({ error, contents }) => {
+  const options = parseUserArgs(cmdLineArgs);
+  sort(options, stdin, fs.createReadStream, ({error, contents}) => {
     stdout.write(contents);
     stderr.write(error);
   });
